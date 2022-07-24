83b74fe360b509fa43197a0c5587cd14802bc332
Utrext <intechros@gmail.com>
2022-07-24 19:22:04 +0300

    Update task.js

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,33 @@
+
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");
+
+function getHole(index) {
+	return document.getElementById(`hole${index}`);
+}
+
+for (let i = 1; i <= 9; i++) {
+	
+	let game = getHole(i);
+	let holeContains = getHole(i).className.includes("hole_has-mole");
+  
+	game.onclick = function () {
+	 
+		if(holeContains)
+			dead.textContent++;
+		else 
+			lost.textContent++;
+		
+			
+		if(dead.textContent == 10) {
+			alert("Вы выиграли!");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		} 
+		else if (lost.textContent == 5) {
+			alert("Вы проиграли!");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		}
+	}
+}
