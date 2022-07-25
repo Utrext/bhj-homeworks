28b9979d08536536376486df69eb823475c891e9
Utrext <intechros@gmail.com>
2022-07-25 19:07:31 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,11 +4,8 @@ const click = document.getElementById("clicker__counter");
 
 function changeSize () {
 	
-	if(img.width === 300) 
-		img.width = 200;
-	else 
-		img.width = 300;
-	
+	img.width = img.width === 300 ? 200 : 300;
+
 	click.textContent++;
 }
 
