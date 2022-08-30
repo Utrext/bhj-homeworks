7188d04c8b5d693de3da931d46d31a93cdb8c195
Utrext <intechros@gmail.com>
2022-08-30 18:27:34 +0300

    Update task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,18 +1,36 @@
-const interestsActive = document.querySelectorAll(".interests_active");
 
-for (let i = 0; i < interestsActive.length; i++) {
-	let generalCheckBox = interestsActive[i].closest(".interest").firstElementChild.firstElementChild;
-	let items = interestsActive[i].querySelectorAll(".interest__check");
+const inputChecked = document.querySelectorAll(".interest__check");
 
-	generalCheckBox.addEventListener("change", function() {
-		for (let i = 0; i < items.length; i++) {
-			let item = items[i];
-			
-			if(this.checked) {
-				item.checked = true;
-			} else {
-				item.checked = false;
-			}
-		}
-	})	
+for (let checked of inputChecked) {
+    
+	checked.addEventListener("change", function (e) {
+        
+		let counter = 0;
+        
+		if (this.closest("ul.interests_active") == null) {
+            
+			for (let input of this.closest("li.interest").querySelectorAll(".interest__check")) {
+                
+				if (e.target.checked == false) {
+                    input.checked = false;
+                } else {
+                    input.checked = true;
+                }
+            }
+        } else {
+            
+			for (let input of this.closest("ul.interests").querySelectorAll(".interest__check")) {
+                
+				if (input.checked == true) {
+                    counter++;
+                }
+            }
+            
+			if (counter == this.closest("ul.interests").children.length) {
+                this.closest("ul.interests").closest("li.interest").querySelector("input").checked = true;
+            } else {
+                this.closest("ul.interests").closest("li.interest").querySelector("input").checked = false;
+            }
+        }
+    })
 }
